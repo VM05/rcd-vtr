783e1bb59bb46365988fd7c9a4dcd8461e2e5c48
victor Montiel <victorhmg05@gmail.com>
2023-05-08 23:32:03 -0400

    modificacion en carruseles

--- js/carrusel-videos.js
+++ js/carrusel-videos.js
@@ -55,6 +55,22 @@ document.addEventListener('DOMContentLoaded', () => {
   firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 });
 
+var $owl = $('.carrusel-peliculas .owl-carousel');
+var $owl2 = $('#carrusel-principal .owl-carousel');
+$owl.children().each(function (index) {
+  $(this).attr('data-hash', index); // NB: .attr() instead of .data()
+});
+
+$owl2.children().each(function (index) {
+  $(this).attr('data-hash', index); // NB: .attr() instead of .data()
+});
+
+$(document).on('click', '.owl-item>div', function () {
+  // see https://owlcarousel2.github.io/OwlCarousel2/docs/api-events.html#to-owl-carousel
+  var $speed = 300; // in ms
+  $owl.trigger('to.owl.carousel', [$(this).data('hash'), $speed]);
+});
+
 //   document.addEventListener('hidden.bs.modal',() =>{
 //     const iframeYoutube = document.querySelector('#ytplayer');
 //     iframeYoutube.src ="";
